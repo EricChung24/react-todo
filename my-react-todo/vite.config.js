a666823df267ed697876553bd9c55ddc58648ba1
EricChung <lf2net67983@gmail.com>
2026-02-25 23:23:49 +0800

    --

--- my-react-todo/vite.config.js
+++ my-react-todo/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// ⚠️ base 要填你的 Repo 名稱
+// 例如 repo 是 https://github.com/xxx/my-site
+// base 就是 "/my-site/"
 export default defineConfig({
+  base: '/react-todo/',
   plugins: [react()],
 })
